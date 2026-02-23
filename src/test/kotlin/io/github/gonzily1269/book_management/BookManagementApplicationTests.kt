bfeee9b4593b598e45a30a625f85dd88916750b6
gonzily1269 <gonzily1269@gmail.com>
2026-02-23 22:40:07 +0900

    first commit

---
+++ src/test/kotlin/io/github/gonzily1269/book_management/BookManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.gonzily1269.book_management
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookManagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
